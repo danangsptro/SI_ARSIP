4306df5f3dff6dc240828185715ac09bc65d6a1a
danangsptro <danangsaputro300@gmail.com>
2021-10-31 23:38:52 +0700

    add auth

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::prefix('admin')->group(function(){
     Route::get('/', 'backend\dashboardController@index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
